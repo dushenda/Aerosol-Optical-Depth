1b3a42ad24d0a29597d6a9336c919c4bba123678
dushenda <dushenda1996@163.com>
2018-11-24 00:28:31 +0800

    Add files via upload

---
+++ SunPosition/start.m
@@ -0,0 +1,110 @@
+function varargout = start(varargin)
+% START MATLAB code for start.fig
+%      START, by itself, creates a new START or raises the existing
+%      singleton*.
+%
+%      H = START returns the handle to a new START or the handle to
+%      the existing singleton*.
+%
+%      START('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in START.M with the given input arguments.
+%
+%      START('Property','Value',...) creates a new START or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before start_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to start_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help start
+
+% Last Modified by GUIDE v2.5 23-Nov-2018 21:10:07
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @start_OpeningFcn, ...
+                   'gui_OutputFcn',  @start_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before start is made visible.
+function start_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to start (see VARARGIN)
+
+% Choose default command line output for start
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes start wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = start_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in A1.
+function A1_Callback(hObject, eventdata, handles)
+% hObject    handle to A1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+    set(gcf,'Visible','off');
+    A1('Visible','on');
+
+
+% --- Executes on button press in A4.
+function A4_Callback(hObject, eventdata, handles)
+% hObject    handle to A4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in A5.
+function A5_Callback(hObject, eventdata, handles)
+% hObject    handle to A5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in A2.
+function A2_Callback(hObject, eventdata, handles)
+% hObject    handle to A2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in A3.
+function A3_Callback(hObject, eventdata, handles)
+% hObject    handle to A3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
